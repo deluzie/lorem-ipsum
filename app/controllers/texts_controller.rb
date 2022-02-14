a45a6e2e7c899adc755eb14b829cdb882135362c
Luzie Deubel <luzie@deubels.de>
2022-02-14 17:43:01 +0100

    adding routes and links to navbar

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -1,5 +1,17 @@
 class TextsController < ApplicationController
-	def index
-    	@texts = Text.all
- 	end
+	# def index
+ #    	@texts = Text.all
+ # 	end
+
+ 	def show
+    # content_ids = Content.where("
+    #   SELECT *
+    #   FROM contents
+    #   WHERE id < (SELECT MAX(id) FROM Contents)
+    # ")
+
+    # @content.id = content_ids.sample
+    # @content = Content.find(@content_id)
+    	@text = Text.find(Text.pluck(:id).sample)
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # devise_for :users
-  root to: 'pages#home'
-  get "texts", to: "texts#index"
+  root to: 'texts#show'
+  resources :texts, only: [:show]
+  get "/about", to: "pages#about"
+  get "/legal-notice", to: "pages#legal_notice"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
