c12f3f0aaa16ce4ce7886d4f75b3c38ecb47b29c
Luzie Deubel <luzie@deubels.de>
2022-02-15 18:34:38 +0100

    adding icon images

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -1,17 +1,5 @@
 class TextsController < ApplicationController
-	# def index
- #    	@texts = Text.all
- # 	end
-
  	def show
-    # content_ids = Content.where("
-    #   SELECT *
-    #   FROM contents
-    #   WHERE id < (SELECT MAX(id) FROM Contents)
-    # ")
-
-    # @content.id = content_ids.sample
-    # @content = Content.find(@content_id)
       @home_page = true
     	@text = Text.find(Text.pluck(:id).sample)
 	end
