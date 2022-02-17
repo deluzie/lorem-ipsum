99ecfbd95df1a5261a6dde4adcf46d10acf34b46
Luzie Deubel <luzie@deubels.de>
2022-02-17 11:48:31 +0100

    deactivated stimulus

--- app/javascript/controllers/clipboard_controller.js
+++ app/javascript/controllers/clipboard_controller.js
@@ -1,15 +1,15 @@
 import { Controller } from "stimulus";
 
-export default class extends Controller {
-  static targets = ["input"];
-  static values = {
-    feedbackText: String
-  }
+ export default class extends Controller {
+   static targets = ["input"];
+   static values = {
+     feedbackText: String
+   }
 
-  copy(event) {
-    this.inputTarget.select();
-    document.execCommand('copy');
-    event.currentTarget.disabled = true;
-    event.currentTarget.innerText = this.feedbackTextValue;
-  }
-}
+   copy(event) {
+     this.inputTarget.select();
+     document.execCommand('copy');
+     event.currentTarget.disabled = true;
+     event.currentTarget.innerText = this.feedbackTextValue;
+   }
+ }
