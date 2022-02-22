347a6cf3b3be41623b68e00d29fdee7475327b5f
Luzie Deubel <luzie@deubels.de>
2022-02-22 14:19:25 +0100

    changed background color to green

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -29,8 +29,8 @@ import "bootstrap"
 
 var colors = ['#E5D3FF', '#FFF5CE', '#9750FF', '#FFCDB0', '#FFD3FE', '#D3D8FF', '#93AAFF'];
 
-var changeBackground = function() {
-    document.body.style.background = colors[Math.floor(Math.random()*colors.length)];
+var changeColor = function() {
+    document.body.style.color = colors[Math.floor(Math.random()*colors.length)];
 };
 
-changeBackground();
+changeColor();
